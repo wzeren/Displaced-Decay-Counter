1e644d76d7d34bd3f9e971f81d1a24ae7085a913
wzeren <wzeren@users.noreply.github.com>
2025-11-19 01:26:45 +0000

    deploy: 5592626ee2cf541df6b88d303fdc21527c74e840

--- search/files_3.js
+++ search/files_3.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['delighta_2ecc_0',['DELIGHTA.cc',['../d3/d41/DELIGHTA_8cc.html',1,'']]],
   ['delighta_2eh_1',['DELIGHTA.h',['../d2/d8a/DELIGHTA_8h.html',1,'']]],
-  ['detectors_2ecc_2',['detectors.cc',['../d8/dcb/detectors_8cc.html',1,'']]],
-  ['detectors_2eh_3',['detectors.h',['../d4/d73/detectors_8h.html',1,'']]],
-  ['deteditor_2ecxx_4',['DetEditor.cxx',['../d0/d33/DetEditor_8cxx.html',1,'']]]
+  ['delightb_2ecc_2',['DELIGHTB.cc',['../da/d52/DELIGHTB_8cc.html',1,'']]],
+  ['delightb_2eh_3',['DELIGHTB.h',['../d4/d30/DELIGHTB_8h.html',1,'']]],
+  ['detectors_2ecc_4',['detectors.cc',['../d8/dcb/detectors_8cc.html',1,'']]],
+  ['detectors_2eh_5',['detectors.h',['../d4/d73/detectors_8h.html',1,'']]],
+  ['deteditor_2ecxx_6',['DetEditor.cxx',['../d0/d33/DetEditor_8cxx.html',1,'']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -3,8 +3,10 @@ var searchData=
   ['decprob_0',['DecProb',['../de/d8c/classCylSeg.html#a7e46c2aa63dc333d581d025f67b34031',1,'CylSeg']]],
   ['delighta_1',['DELIGHTA',['../d2/d8a/DELIGHTA_8h.html#ac8be3e8a88909e9045bea216f0c8e547',1,'DELIGHTA():&#160;DELIGHTA.cc'],['../d3/d41/DELIGHTA_8cc.html#ac8be3e8a88909e9045bea216f0c8e547',1,'DELIGHTA():&#160;DELIGHTA.cc']]],
   ['delightacuts_2',['DELIGHTACuts',['../d2/d8a/DELIGHTA_8h.html#ae8aa11f9e342a26fefa4419f6a583191',1,'DELIGHTACuts(HepMC::GenEvent *evtInput):&#160;DELIGHTA.cc'],['../d3/d41/DELIGHTA_8cc.html#a7b3df7e41daf4f97101f6e7ab3d287d0',1,'DELIGHTACuts(HepMC::GenEvent *evtin):&#160;DELIGHTA.cc']]],
-  ['detacc_3',['DetAcc',['../d5/da2/classDetector.html#a8996790875b78ededf8a430bb1a73ce3',1,'Detector']]],
-  ['detector_4',['Detector',['../d5/da2/classDetector.html#a7c91502624f5f650e44762757567d913',1,'Detector']]],
-  ['die_5',['die',['../d4/dbf/main_8h.html#af72862963d72fc8d093feda4392688cd',1,'die(std::string output):&#160;main.cc'],['../d9/d0f/main_8cc.html#af72862963d72fc8d093feda4392688cd',1,'die(std::string output):&#160;main.cc']]],
-  ['docalculations_6',['doCalculations',['../d4/db0/classanalysis.html#a576888a49b786d607fcb001037ded317',1,'analysis']]]
+  ['delightb_3',['DELIGHTB',['../d4/d30/DELIGHTB_8h.html#a132f5036c2cbcff21be394b8b4e97e68',1,'DELIGHTB():&#160;DELIGHTB.cc'],['../da/d52/DELIGHTB_8cc.html#a132f5036c2cbcff21be394b8b4e97e68',1,'DELIGHTB():&#160;DELIGHTB.cc']]],
+  ['delightbcuts_4',['DELIGHTBCuts',['../d4/d30/DELIGHTB_8h.html#a7903639a855756799d38c650bd0b7986',1,'DELIGHTBCuts(HepMC::GenEvent *evtInput):&#160;DELIGHTB.cc'],['../da/d52/DELIGHTB_8cc.html#a4100ea41004aa412094c0aaf2fdeeea6',1,'DELIGHTBCuts(HepMC::GenEvent *evtin):&#160;DELIGHTB.cc']]],
+  ['detacc_5',['DetAcc',['../d5/da2/classDetector.html#a8996790875b78ededf8a430bb1a73ce3',1,'Detector']]],
+  ['detector_6',['Detector',['../d5/da2/classDetector.html#a7c91502624f5f650e44762757567d913',1,'Detector']]],
+  ['die_7',['die',['../d4/dbf/main_8h.html#af72862963d72fc8d093feda4392688cd',1,'die(std::string output):&#160;main.cc'],['../d9/d0f/main_8cc.html#af72862963d72fc8d093feda4392688cd',1,'die(std::string output):&#160;main.cc']]],
+  ['docalculations_8',['doCalculations',['../d4/db0/classanalysis.html#a576888a49b786d607fcb001037ded317',1,'analysis']]]
 ];
